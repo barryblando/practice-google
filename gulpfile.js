2ae3ffcb2e0fdfd3ea5a1af20a863e1e4bf23e01
heartless007 <barryblando@gmail.com>
2017-06-03 22:20:06 +0800

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+/**
+ * Created by heartless007 on 5/30/2017.
+ */
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      browserSync = require('browser-sync').create();
+
+function handleError(error) {
+  console.log(error.toString());
+  this.emit('end')
+}
+
+gulp.task('styles', function () {
+  gulp.src('./src/scss/application.scss')
+      .pipe(sass())
+      .on('error', handleError)
+      .pipe(gulp.dest('./src/css'))
+      .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('serve', function () {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  gulp.watch('./src/scss/**/*.scss', ['styles']);
+  gulp.watch('./*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['styles', 'serve']);
